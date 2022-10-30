6d25aab7a16714cc1b4279e1fad6c780e661b81b
hyotty <ht19960109@gmail.com>
2022-10-30 16:01:40 +0900

    fix code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //dotenvの適用
 require('dotenv').config();
 //.envからtokenの呼び出し
-const {token} = process.env;
+const token = process.env.TOKEN;
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions]
